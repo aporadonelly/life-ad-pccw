ec4a43188ea65721f7e20b3563764c0e79aff85f
pccw-nellyaporado <NellyRose.T.Aporado@pccw.com>
2021-04-13 09:38:48 +0800

    Added Employee page to App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import theme from './ui/theme';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './ui/layout';
 import Dashboard from './components/dashboard/dashboard';
+import Employees from './pages/Employee/components/EmployeeSearch/Employees';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-        <Router>
-          <Layout>
-             <Dashboard />
-          </Layout>
-        </Router>
+      <Router>
+        <Layout>
+          <Dashboard />
+          <Employees />
+        </Layout>
+      </Router>
     </ThemeProvider>
   );
 }
