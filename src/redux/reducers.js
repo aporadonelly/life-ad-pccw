08abf44cf01508f45716f5418ccab90af1612c94
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-05 22:57:50 +0800

    add employees reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
+import employeesReducer from "@reducers/employeesReducer";
 import { userReducer } from "./features/user/reducer";
 
 const rootReducer = combineReducers({
+  employees: employeesReducer,
   user: userReducer,
 });
 
