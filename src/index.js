c751d0a6fb9f737c87687049c6c359238b0c95de
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-05 22:48:10 +0800

    refactor + implement CssBaseline

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { I18nextProvider } from "react-i18next";
-import { persistor, store } from "./store";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+import configureAppStore from "./redux/configureAppStore";
 import i18n from "./i18n";
+import theme from "./styles/theme";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+const { store, persistor } = configureAppStore({});
 
 ReactDOM.render(
   <React.Suspense fallback={null}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <I18nextProvider i18n={i18n}>
-          <BrowserRouter>
-            <App />,
-          </BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Router>
+              <CssBaseline />
+              <App />
+            </Router>
+          </ThemeProvider>
         </I18nextProvider>
       </PersistGate>
     </Provider>
   </React.Suspense>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
