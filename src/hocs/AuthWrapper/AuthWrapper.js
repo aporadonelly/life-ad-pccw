02dda72c23236bf5a0134f19f2e7f8ae5aa4385e
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-12 17:10:12 +0800

    add route pathname validation

--- src/hocs/AuthWrapper/AuthWrapper.js
+++ src/hocs/AuthWrapper/AuthWrapper.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { userSelector } from "@redux/features/user/selectors";
 
 const AuthWrapper = (WrappedComponent) => {
   const Wrapper = ({ user, ...props }) => {
+    const { pathname } = useLocation();
+
     useEffect(() => {
-      if (!user) {
+      if (!user && pathname !== process.env.REACT_APP_REDIRECT_URL) {
         window.location.href = process.env.REACT_APP_REDIRECT_URL;
       }
-    }, [user]);
+    }, [user, pathname]);
 
     if (!user) return null;
 
