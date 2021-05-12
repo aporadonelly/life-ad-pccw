ff13bd146008f79189f8b07346e63db761b40767
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-12 18:08:52 +0800

    Add array list of languages

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,29 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
+import { map } from "lodash";
+
+export const languages = [
+  {
+    code: "en-US",
+    lang: "English",
+  },
+  {
+    code: "zh-CN",
+    lang: "Chinese (Simplified)",
+  },
+  {
+    code: "zh-HK",
+    lang: "Chinese (Traditional)",
+  },
+];
 
 i18n.use(LanguageDetector);
 i18n.use(HttpApi);
 i18n.init({
   debug: false,
   fallbackLng: "en-US",
-  whitelist: ["en-US", "zh-CN", "zh-HK"],
+  whitelist: map(languages, "code"),
   backend: {
     loadPath: `${process.env.PUBLIC_URL}/locales/i18n/{{lng}}/{{ns}}.json`,
   },
