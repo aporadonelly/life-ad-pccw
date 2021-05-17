e16f13981540f12e76701e0c131d9550f5d2c3eb
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-17 16:42:34 +0800

    Apply nested routing

--- src/pages/Employer/Employer.js
+++ src/pages/Employer/Employer.js
@@ -1,42 +1,15 @@
-import { Grid, Card, CardContent, Typography } from "@material-ui/core";
+import { Switch, Route } from "react-router-dom";
+import CompanyProfile from "./CompanyProfile";
+import ViewProfile from "./ViewProfile";
 
-const Employer = () => {
+const Employer = (props) => {
+  const path = props.match.path;
+
   return (
-    
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Card>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="h6" color="primary">
-                  Member Enquiry
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                Content
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="h6" color="primary">
-                  Search Result
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                Content
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+    <Switch>
+      <Route exact path={`${path}`} component={CompanyProfile} />
+      <Route path={`${path}/profile`} component={ViewProfile} />
+    </Switch>
   );
 };
 
