9065c2362ec0ed5b7bc90cf4fe1dff24767e9c85
archy0528 <83213226+archy0528@users.noreply.github.com>
2021-05-18 12:59:18 +0800

    clean up code

--- src/components/dialogs/DialogBox/DialogBox.js
+++ src/components/dialogs/DialogBox/DialogBox.js
@@ -1,39 +1,3 @@
-// import { useStyles } from "./styles";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogContentText,
-//   Button,
-//   IconButton,
-// } from "@material-ui/core";
-// import CancelIcon from "@material-ui/icons/Cancel";
-
-// const DialogBox = ({ open, onClose }) => {
-//   const classes = useStyles();
-
-//   return (
-//     <Dialog
-//       PaperProps={{ className: classes.paper, elevation: 0 }}
-//       maxWidth="xs"
-//       open={true}
-//       onClose={onClose}
-//     >
-//       <IconButton className={classes.closeButton}>
-//         <CancelIcon fontSize="large" color="inherit" />
-//       </IconButton>
-//       <DialogContent className={classes.content}>
-//         <DialogContentText id="alert-dialog-description">
-//           Let Google help apps determine location. This means sending anonymous
-//           location data to Google, even when no apps are running.
-//         </DialogContentText>
-//         <Button size="small">Ok</Button>
-//       </DialogContent>
-//     </Dialog>
-//   );
-// };
-
-// export default DialogBox;
-
 import { useStyles } from "./styles";
 import { Backdrop, Container, Paper, ButtonBase } from "@material-ui/core";
 import { Cancel as CancelIcon } from "@material-ui/icons";
