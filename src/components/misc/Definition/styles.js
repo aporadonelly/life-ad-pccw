9a58de878d2418b4653b8f64afd09dcb2617bfcd
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-19 12:05:46 +0800

    Changed fontWeight style

--- src/components/misc/Definition/styles.js
+++ src/components/misc/Definition/styles.js
@@ -1,8 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  fontWeightBold: {
-    fontWeight: theme.typography.fontWeightBold,
+  fontWeight: {
+    fontWeight: theme.typography.fontWeightMedium,
   },
 }));
 
