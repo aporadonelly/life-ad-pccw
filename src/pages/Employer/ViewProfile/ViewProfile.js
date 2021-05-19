471b6f78d783b916c3a31b302d153ad9ca25c2ab
pccw-nellyaporado <NellyRose.T.Aporado@pccw.com>
2021-05-19 12:00:12 +0800

    Refactored using Definition

--- src/pages/Employer/ViewProfile/ViewProfile.js
+++ src/pages/Employer/ViewProfile/ViewProfile.js
@@ -3,24 +3,9 @@ import { PageInner } from "@components/layout";
 import { useTranslation } from "react-i18next";
 import eSig from "../../../assets/icons/signature.svg";
 import { useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  dd: {
-    fontSize: "16px",
-    marginBottom: "10px",
-  },
-  label: {
-    fontSize: "13px",
-  },
-  backBtn: {
-    backgroundColor: "#EF841F",
-    color: "#fff",
-  },
-}));
+import { Definition } from "@components/misc";
 
 const ViewProfile = () => {
-  const classes = useStyles();
   const history = useHistory();
   const { t } = useTranslation(["typography", "form", "button"]);
   return (
@@ -29,203 +14,84 @@ const ViewProfile = () => {
         <Grid item xs={12}>
           <Card>
             <CardContent>
-              <Grid
-                container
-                component="dl"
-                spacing={1}
-                alignItems="flex-start"
-              >
-                <Grid item xs={12}>
-                  <Typography variant="h6" color="primary">
-                    {t("typography:heading.authorizedPerson")}
-                  </Typography>
-                </Grid>
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    color="textSecondary"
-                    className={classes.label}
-                  >
-                    {t("form:label.idType")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    HKID
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.idNumber")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Y371385(4)
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.birthdate")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    1970/04/21
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.nationality")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Hong Kong
-                  </Typography>
-                </Grid>
-
-                {/* Second line */}
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.chineseLastName")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    听
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.chineseFirstName")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    耐莉
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.lastName")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Aporado
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.firstName")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Nelly
-                  </Typography>
-                </Grid>
-
-                {/* Third line */}
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.title")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Ms.
-                  </Typography>
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.jobTitle")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Accountant
-                  </Typography>
-                </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6" color="primary">
+                  {t("typography:heading.authorizedPerson")}
+                </Typography>
               </Grid>
+              <Definition spacing={2} xs={3}>
+                <Definition.List>
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.idType")}
+                    dd="HKID"
+                  />
+
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.idNumber")}
+                    dd="Y371385(4)"
+                  />
+
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.birthdate")}
+                    dd=" 1970/04/21"
+                  />
+
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.nationality")}
+                    dd=" Hong Kong"
+                  />
+
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.chineseLastName")}
+                    dd=" 听"
+                  />
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.chineseFirstName")}
+                    dd="  耐莉"
+                  />
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.lastName")}
+                    dd="Aporado"
+                  />
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.firstName")}
+                    dd="  Nelly"
+                  />
+
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.title")}
+                    dd="Ms."
+                  />
+                  <Definition.Item
+                    item
+                    xs={3}
+                    dt={t("form:label.jobTitle")}
+                    dd="Accountant"
+                  />
+                </Definition.List>
+              </Definition>
             </CardContent>
           </Card>
         </Grid>
+
         <Grid item xs={12}>
           <Card>
             <CardContent>
@@ -240,57 +106,28 @@ const ViewProfile = () => {
                     {t("form:label.address")}
                   </Typography>
                 </Grid>
-                <Grid item xs={6}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.registeredOfcAddress")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Rm 307, Man Tai Building, 31 Lok Man Street, Tai Po, NT.
-                  </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.businessAddress")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Same as Correspondence Address
-                  </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography
-                    component="dt"
-                    variant="body1"
-                    className={classes.label}
-                    color="textSecondary"
-                  >
-                    {t("form:label.correspondenceAddress")}
-                  </Typography>
-                  <Typography
-                    component="dd"
-                    variant="h6"
-                    className={classes.dd}
-                  >
-                    Same as Correspondence Address
-                  </Typography>
-                </Grid>
+                <Definition spacing={2} xs={6}>
+                  <Definition.List>
+                    <Definition.Item
+                      item
+                      xs={6}
+                      dt={t("form:label.registeredOfcAddress")}
+                      dd=" Rm 307, Man Tai Building, 31 Lok Man Street, Tai Po, NT."
+                    />
+                    <Definition.Item
+                      item
+                      xs={6}
+                      dt={t("form:label.businessAddress")}
+                      dd=" Same as Correspondence Address"
+                    />
+                    <Definition.Item
+                      item
+                      xs={6}
+                      dt={t("form:label.correspondenceAddress")}
+                      dd=" Same as Correspondence Address"
+                    />
+                  </Definition.List>
+                </Definition>
               </Grid>
             </CardContent>
           </Card>
@@ -312,7 +149,9 @@ const ViewProfile = () => {
                 <Grid item xs={6}>
                   <img
                     src={eSig}
-                    style={{ width: "20em" }}
+                    style={{
+                      width: "20em",
+                    }}
                     alt="caret left icon"
                   />
                 </Grid>
@@ -325,7 +164,6 @@ const ViewProfile = () => {
             <Button
               data-testid="back-btn"
               onClick={() => history.push("/employer")}
-              className={classes.backBtn}
             >
               {t("button:backToCompanyProfile")}
             </Button>
