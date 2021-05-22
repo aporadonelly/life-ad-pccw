cb5c189ac30fca38482f4e901574e7fcd155bfff
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-22 14:09:56 +0800

    Add array of app routes

---
+++ src/routes/appRoutes.js
@@ -0,0 +1,51 @@
+import { Members } from "@pages";
+
+const appRoutes = [
+  {
+    name: "Task",
+    path: "/task",
+    component: null,
+  },
+  {
+    name: "Member",
+    path: "/members",
+    component: Members,
+  },
+  {
+    name: "Individual",
+    path: "/individual",
+    component: null,
+  },
+  {
+    name: "Employer",
+    path: "/employers",
+    component: null,
+  },
+  {
+    name: "Company",
+    path: "/companies",
+    component: null,
+  },
+  {
+    name: "Trustee",
+    path: "/trustees",
+    component: null,
+  },
+  {
+    name: "Transaction History",
+    path: "/transactions",
+    component: null,
+  },
+  {
+    name: "Instruction",
+    path: "/instructions",
+    component: null,
+  },
+  {
+    name: "Configuration",
+    path: "/config",
+    component: null,
+  },
+];
+
+export default appRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+export { default as appRoutes } from "./appRoutes";
+export { default as membersRoutes } from "./membersRoutes";
+export { default as sidebarRoutes } from "./sidebarRoutes";
