3d23612b3fb9c0eafe5afde2b9bbe08efa789e39
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-24 09:26:42 +0800

    Changed file name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,3 @@
-export { default as appRoutes } from "./appRoutes";
-export { default as membersRoutes } from "./membersRoutes";
-export { default as sidebarRoutes } from "./sidebarRoutes";
+export { default as appRoutes } from "./app.routes";
+export { default as membersRoutes } from "./members.routes";
+export { default as sidebarRoutes } from "./sidebar.routes";
