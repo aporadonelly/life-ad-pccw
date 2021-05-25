c9d470feaf952cc343e03405abcdf3b7a3afd24f
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-05-25 16:32:04 +0800

    Add missing logout button

--- src/containers/Page/Header/UserMenu/UserMenu.js
+++ src/containers/Page/Header/UserMenu/UserMenu.js
@@ -1,25 +1,76 @@
+import { useRef, useState, useEffect } from "react";
 import { useStyles } from "./styles";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+} from "@material-ui/core";
 import { UserPlaceholder as UserPlaceholderIcon } from "@components/icons";
+import { PopoverMenu } from "@components/menus";
 
 const UserMenu = (props) => {
-  const { displayName } = props;
+  const { displayName, onLogout } = props;
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+  const prevOpen = useRef(open);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef?.current?.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
-    <List disablePadding dense>
-      <ListItem>
-        <ListItemIcon className={classes.avatar}>
-          <UserPlaceholderIcon />
-        </ListItemIcon>
-        <ListItemText
-          className={classes.info}
-          primary={displayName}
-          secondary="Admin Operator"
-          secondaryTypographyProps={{ variant: "caption", color: "inherit" }}
-        />
-      </ListItem>
-    </List>
+    <>
+      <List disablePadding dense>
+        <ListItem ref={anchorRef} onClick={handleToggle} button>
+          <ListItemIcon className={classes.avatar}>
+            <UserPlaceholderIcon />
+          </ListItemIcon>
+          <ListItemText
+            className={classes.info}
+            primary={displayName}
+            secondary="Admin Operator"
+            secondaryTypographyProps={{ variant: "caption", color: "inherit" }}
+          />
+        </ListItem>
+      </List>
+      <PopoverMenu
+        ref={anchorRef}
+        open={open}
+        onClose={handleClose}
+        onListKeyDown={handleListKeyDown}
+        PaperProps={{ className: classes.paper }}
+      >
+        <MenuItem dense onClick={onLogout}>
+          Logout
+        </MenuItem>
+      </PopoverMenu>
+    </>
   );
 };
 
--- src/containers/Page/Header/UserMenu/styles.js
+++ src/containers/Page/Header/UserMenu/styles.js
@@ -11,6 +11,10 @@ const useStyles = makeStyles((theme) => ({
   info: {
     marginTop: 4,
   },
+  paper: {
+    minWidth: 130,
+    boxShadow: "0px 3px 6px #00000029",
+  },
 }));
 
 export { useStyles };
