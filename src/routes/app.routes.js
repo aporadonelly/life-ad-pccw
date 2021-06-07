91c1290dbeb9aea8bc9b323cf02b290c15841f70
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-07 10:08:16 +0800

    Update app routes

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,49 +1,57 @@
-
 const appRoutes = [
   {
     name: "Task",
     path: "/task",
     component: null,
+    tab: false,
   },
   {
     name: "Member",
     path: "/members",
     component: null,
+    tab: false,
   },
   {
     name: "Individual",
     path: "/individual",
     component: null,
+    tab: false,
   },
   {
     name: "Employer",
     path: "/employers",
     component: null,
+    tab: false,
   },
   {
     name: "Company",
     path: "/companies",
     component: null,
+    tab: false,
   },
   {
     name: "Trustee",
     path: "/trustees",
     component: null,
+    tab: false,
   },
   {
     name: "Transaction History",
     path: "/transactions",
     component: null,
+    tab: false,
   },
   {
     name: "Instruction",
     path: "/instructions",
     component: null,
+    tab: false,
   },
   {
     name: "Configuration",
     path: "/config",
     component: null,
+    tab: false,
   },
 ];
 
