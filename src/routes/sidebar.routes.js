7cfbd16cc32be14645b794824096217969b5a1a4
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-07 15:48:46 +0800

    Remove comments

--- src/routes/sidebar.routes.js
+++ src/routes/sidebar.routes.js
@@ -10,15 +10,15 @@ import {
 } from "@material-ui/icons";
 
 const sidebarRoutes = [
-  // {
-  //   name: "task",
-  //   path: "/task",
-  //   icon: <HomeOutlinedIcon />,
-  //   component: null,
-  // },
-  // {
-  //   divider: true,
-  // },
+  {
+    name: "task",
+    path: "/task",
+    icon: <HomeOutlinedIcon />,
+    component: null,
+  },
+  {
+    divider: true,
+  },
   {
     name: "member",
     path: "/members",
@@ -32,48 +32,48 @@ const sidebarRoutes = [
     icon: <PersonOutlineOutlinedIcon />,
     component: null,
   },
-  // {
-  //   divider: true,
-  // },
-  // {
-  //   name: "employer",
-  //   path: "/employers",
-  //   icon: <BusinessCenterOutlinedIcon />,
-  //   component: null,
-  // },
-  // {
-  //   name: "company",
-  //   path: "/companies",
-  //   icon: <BusinessOutlinedIcon />,
-  //   component: null,
-  // },
-  // {
-  //   divider: true,
-  // },
-  // {
-  //   name: "trustee",
-  //   path: "/trustees",
-  //   icon: <AccountBalanceOutlinedIcon />,
-  //   component: null,
-  // },
-  // {
-  //   name: "transactionHistory",
-  //   path: "/transactions",
-  //   icon: <HistoryIcon />,
-  //   component: null,
-  // },
-  // {
-  //   name: "instruction",
-  //   path: "/instructions",
-  //   icon: <InfoOutlinedIcon />,
-  //   component: null,
-  // },
-  // {
-  //   name: "configuration",
-  //   path: "/config",
-  //   icon: <BusinessOutlinedIcon />,
-  //   component: null,
-  // },
+  {
+    divider: true,
+  },
+  {
+    name: "employer",
+    path: "/employers",
+    icon: <BusinessCenterOutlinedIcon />,
+    component: null,
+  },
+  {
+    name: "company",
+    path: "/companies",
+    icon: <BusinessOutlinedIcon />,
+    component: null,
+  },
+  {
+    divider: true,
+  },
+  {
+    name: "trustee",
+    path: "/trustees",
+    icon: <AccountBalanceOutlinedIcon />,
+    component: null,
+  },
+  {
+    name: "transactionHistory",
+    path: "/transactions",
+    icon: <HistoryIcon />,
+    component: null,
+  },
+  {
+    name: "instruction",
+    path: "/instructions",
+    icon: <InfoOutlinedIcon />,
+    component: null,
+  },
+  {
+    name: "configuration",
+    path: "/config",
+    icon: <BusinessOutlinedIcon />,
+    component: null,
+  },
 ];
 
 export default sidebarRoutes;
