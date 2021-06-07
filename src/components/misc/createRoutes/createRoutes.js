722f19c41fb5ff2c30cfb118c06c46e35be89d95
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-07 10:38:15 +0800

    Use hoc AuthWrapper

--- src/components/misc/createRoutes/createRoutes.js
+++ src/components/misc/createRoutes/createRoutes.js
@@ -1,23 +1,25 @@
 import { Switch, Route } from "react-router-dom";
-import createPrivateRoute from "./createPrivateRoute";
+import { AuthWrapper } from "@hocs";
 
 const createRoutes = (routes) => {
   const renderRoutes = (routes) =>
-    routes.map((route) =>
-      route.children ? (
-        <Switch>{renderRoutes(route.children)}</Switch>
-      ) : (
+    routes.map((route) => {
+      const wrappedComponent = AuthWrapper(route.component);
+
+      if (route.children) {
+        return <Switch>{renderRoutes(route.children)}</Switch>;
+      }
+
+      return (
         <Route
           key={route.name}
           exact={route.exact}
           name={route.name}
           path={route.path}
-          {...createPrivateRoute({
-            component: route.component,
-          })}
+          component={wrappedComponent}
         />
-      )
-    );
+      );
+    });
 
   return <Switch>{renderRoutes(routes)}</Switch>;
 };
