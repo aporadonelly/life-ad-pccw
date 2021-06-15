1638e14e9e06c66f6c17b6bcfd58b2c41fc5d14c
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-15 11:36:18 +0800

    Remove hover event on sidebar + show tooltip on menu toggler

--- src/containers/Page/MenuToggler/MenuToggler.js
+++ src/containers/Page/MenuToggler/MenuToggler.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+import { useDebounce } from "react-use";
 import { useAppState } from "@contexts/AppProvider";
 import clsx from "clsx";
 import { useStyles } from "./styles";
-import { ButtonBase } from "@material-ui/core";
+import { ButtonBase, Tooltip } from "@material-ui/core";
 import {
   ChevronLeft as ChevronLeftIcon,
   ChevronRight as ChevronRightIcon,
@@ -10,24 +12,36 @@ import {
 const MenuToggler = () => {
   const classes = useStyles();
   const { state, dispatch } = useAppState();
+  const [title, setTitle] = useState();
+
+  useDebounce(
+    () => {
+      const title = state.collapsed ? "Collapse" : "Expand";
+      setTitle(title);
+    },
+    250,
+    [state.collapsed, setTitle]
+  );
 
   return (
-    <ButtonBase
-      className={clsx("offset", classes.menuToggler, {
-        [classes.slideIn]: state.collapsed,
-        [classes.slideOut]: !state.collapsed,
-      })}
-      id="app-menu-toggler"
-      onClick={() =>
-        dispatch({ type: "sidebarCollapsed", collapsed: !state.collapsed })
-      }
-    >
-      {state.collapsed ? (
-        <ChevronLeftIcon color="primary" />
-      ) : (
-        <ChevronRightIcon color="primary" />
-      )}
-    </ButtonBase>
+    <Tooltip title={title} placement="right">
+      <ButtonBase
+        className={clsx("offset", classes.menuToggler, {
+          [classes.slideIn]: state.collapsed,
+          [classes.slideOut]: !state.collapsed,
+        })}
+        id="app-menu-toggler"
+        onClick={() =>
+          dispatch({ type: "sidebarCollapsed", collapsed: !state.collapsed })
+        }
+      >
+        {state.collapsed ? (
+          <ChevronLeftIcon color="primary" />
+        ) : (
+          <ChevronRightIcon color="primary" />
+        )}
+      </ButtonBase>
+    </Tooltip>
   );
 };
 
