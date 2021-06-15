57498fa7869a60d80a32f2faac6698afc25c1388
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-15 11:39:26 +0800

    Changed tooltip color

--- src/containers/Page/MenuToggler/MenuToggler.js
+++ src/containers/Page/MenuToggler/MenuToggler.js
@@ -2,12 +2,22 @@ import { useState } from "react";
 import { useDebounce } from "react-use";
 import { useAppState } from "@contexts/AppProvider";
 import clsx from "clsx";
+import { withStyles } from "@material-ui/core/styles";
 import { useStyles } from "./styles";
 import { ButtonBase, Tooltip } from "@material-ui/core";
 import {
   ChevronLeft as ChevronLeftIcon,
   ChevronRight as ChevronRightIcon,
 } from "@material-ui/icons";
+
+const LightTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: "0px 3px 6px #00000029",
+    fontSize: 11,
+  },
+}))(Tooltip);
 
 const MenuToggler = () => {
   const classes = useStyles();
@@ -24,7 +34,7 @@ const MenuToggler = () => {
   );
 
   return (
-    <Tooltip title={title} placement="right">
+    <LightTooltip title={title} placement="right">
       <ButtonBase
         className={clsx("offset", classes.menuToggler, {
           [classes.slideIn]: state.collapsed,
@@ -41,7 +51,7 @@ const MenuToggler = () => {
           <ChevronRightIcon color="primary" />
         )}
       </ButtonBase>
-    </Tooltip>
+    </LightTooltip>
   );
 };
 
