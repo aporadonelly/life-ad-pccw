ff05fb65b4809c1264513c1053d093bff87df990
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-17 11:30:33 +0800

    update tooltip + sidebar collapse state

---
+++ src/containers/Page/MenuToggler/Tooltip/Tooltip.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useDebounce } from "react-use";
+import { useAppState } from "@contexts/AppProvider";
+import { useTheme } from "@material-ui/core/styles";
+import { useStyles } from "./styles";
+import { Tooltip as MuiTooltip } from "@material-ui/core";
+
+const getTitle = (state) => (state.collapsed ? "Expand" : "Collapse");
+
+const Tooltip = (props) => {
+  const { children } = props;
+  const { state } = useAppState();
+  const [title, setTitle] = useState(() => getTitle(state));
+  const theme = useTheme();
+  const classes = useStyles();
+
+  useDebounce(
+    () => setTitle(getTitle(state)),
+    theme.transitions.duration.shorter,
+    [state.collapsed]
+  );
+
+  return (
+    <MuiTooltip classes={classes} title={title} placement="right">
+      {children}
+    </MuiTooltip>
+  );
+};
+
+export default Tooltip;
