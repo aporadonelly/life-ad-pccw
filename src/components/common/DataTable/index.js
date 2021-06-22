55b709ae1c205ce2c191a4e9b887dc097979009f
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-22 11:53:33 +0800

    Remove unused exports

--- src/components/common/DataTable/index.js
+++ src/components/common/DataTable/index.js
@@ -1,4 +1,1 @@
 export { default } from "./DataTable";
-export { default as QuickSearch } from "./TableQuickSearch";
-export { default as Pagination } from "./TablePagination";
-export { default as ShowEntries } from "./TableShowEntries";
