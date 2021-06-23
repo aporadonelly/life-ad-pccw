7b474eabdd7ec2d1698b79a7f06891b96192e99d
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-23 16:07:44 +0800

    add enrollment employer adapter

---
+++ src/adapters/enrollmentEmployer.js
@@ -0,0 +1,14 @@
+import AxiosAdapter from "./axios.adapter";
+
+class EnrollmentEmployer extends AxiosAdapter {
+  ldCmpnyRltdPrsn(payload) {
+    const { cmpnyPrsnTypId, cmpnyUuid, schmUuid, clntUuid } = payload;
+    return this.instance.get("/ldCmpnyRltdPrsn", {
+      params: { cmpnyUuid, cmpnyPrsnTypId, schmUuid, clntUuid },
+    });
+  }
+}
+
+export default new EnrollmentEmployer({
+  baseURL: process.env.REACT_APP_ENROLLMENT_ER_BASE_URL,
+});
