1ada81fcd9776b1e5f158ce898286e8d68cd0e8d
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-24 16:11:51 +0800

    Update export adapter

--- src/redux/features/enrollmentEmployer/actions.js
+++ src/redux/features/enrollmentEmployer/actions.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createAction } from "@reduxjs/toolkit";
-import { registrationEmployer } from "@adapters";
+import { enrollmentEmployer } from "@adapters";
 
 export const draftEnquiry = createAction("@@empf/reg/er/draftEnquiry");
 
@@ -11,7 +11,7 @@ export const ldSrchCmpny = createAsyncThunk(
   "@@empf/reg/er/ldSrchCmpny",
   async (payload, { rejectWithValue }) => {
     try {
-      const employers = await registrationEmployer.ldSrchCmpny(payload);
+      const employers = await enrollmentEmployer.ldSrchCmpny(payload);
       return { employers };
     } catch (error) {
       return rejectWithValue({ error });
