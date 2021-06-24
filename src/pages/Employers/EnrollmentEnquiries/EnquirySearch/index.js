05685f24d00f0c8d1e535d2c6d33c5bdc399388f
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-24 11:25:39 +0800

    refactor redux module

--- src/pages/Employers/EnrollmentEnquiries/EnquirySearch/index.js
+++ src/pages/Employers/EnrollmentEnquiries/EnquirySearch/index.js
@@ -1,18 +1,22 @@
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { push } from "connected-react-router";
 import { customTypeByGroupIdSelector } from "@redux/features/system/selectors";
 import {
   isLoadingSelector,
   errorSelector,
-  enquirySelector,
-} from "@redux/features/employers/selectors";
-import { getEmployers } from "@redux/features/employers/actions";
+  draftEnquirySelector,
+} from "@redux/features/registrationEmployer/selectors";
+import {
+  ldSrchCmpny,
+  draftEnquiry,
+} from "@redux/features/registrationEmployer/actions";
 
 import EnquirySearch from "./EnquirySearch";
 
 const mapStateToProps = (state) => ({
   isLoading: isLoadingSelector(state),
-  enquiry: enquirySelector(state),
+  enquiry: draftEnquirySelector(state),
   error: errorSelector(state),
   industryType: customTypeByGroupIdSelector(state, "NT"),
   registrationType: customTypeByGroupIdSelector(state, "CI"),
@@ -24,7 +28,9 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   ...bindActionCreators(
     {
-      getEmployers,
+      ldSrchCmpny,
+      draftEnquiry,
+      push,
     },
     dispatch
   ),
--- src/pages/Employers/EnrollmentEnquiries/SearchResult/index.js
+++ src/pages/Employers/EnrollmentEnquiries/SearchResult/index.js
@@ -1,12 +1,17 @@
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import { push } from "connected-react-router";
 import {
-  employersSelector,
   isLoadingSelector,
   errorSelector,
-  enquirySelector,
-} from "@redux/features/employers/selectors";
-import { getEmployers, saveEnquiry } from "@redux/features/employers/actions";
+  draftEnquirySelector,
+  employersSelector,
+} from "@redux/features/registrationEmployer/selectors";
+import {
+  ldSrchCmpny,
+  draftEnquiry,
+  setSelectedPnsnId,
+} from "@redux/features/registrationEmployer/actions";
 
 import SearchResult from "./SearchResult";
 
@@ -14,11 +19,14 @@ const mapStateToProps = (state) => ({
   employers: employersSelector(state),
   isLoading: isLoadingSelector(state),
   error: errorSelector(state),
-  enquiry: enquirySelector(state),
+  enquiry: draftEnquirySelector(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  ...bindActionCreators({ getEmployers, saveEnquiry }, dispatch),
+  ...bindActionCreators(
+    { ldSrchCmpny, draftEnquiry, setSelectedPnsnId, push },
+    dispatch
+  ),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchResult);
