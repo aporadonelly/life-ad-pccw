4ee19422354d8b6cf0b5ae09c27bfb1afef6e138
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-06-29 15:45:32 +0800

    connect component to redux

--- src/pages/Employers/Company/EmployerEnrollmentInformation/AuthorizedPerson/index.js
+++ src/pages/Employers/Company/EmployerEnrollmentInformation/AuthorizedPerson/index.js
@@ -1,1 +1,16 @@
-export { default } from "./AuthorizedPerson";
+import { connect } from "react-redux";
+import {
+  authorizedPersonSelector,
+  authorizedPersonAddressByTypeIdSelector,
+  authorizedPersonPhoneByTypeIdSelector,
+} from "@redux/features/registrationEmployer/selectors";
+import AuthorizedPerson from "./AuthorizedPerson";
+
+const mapStateToProps = (state) => ({
+  authorizedPerson: authorizedPersonSelector(state),
+  telephone: authorizedPersonPhoneByTypeIdSelector(state, "TP_TP"),
+  mobile: authorizedPersonPhoneByTypeIdSelector(state, "TP_MB"),
+  residentialAddress: authorizedPersonAddressByTypeIdSelector(state, "AD_R"),
+});
+
+export default connect(mapStateToProps, null)(AuthorizedPerson);
