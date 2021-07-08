b26e2dcb74d1a735efbe81fce68a74457a76c4b9
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-07-08 16:06:57 +0800

    Refactor

--- src/containers/EnquiryChips/Chip/index.js
+++ src/containers/EnquiryChips/Chip/index.js
@@ -1,1 +1,21 @@
-export { default } from "./Chip";
+import { connect } from "react-redux";
+import {
+  customTypesEntitiesSelector,
+  customTypeByIdentitySelector,
+  countriesEntitiesSelector,
+  trusteesEntitiesSelector,
+  schemesEntitiesSelector,
+} from "@redux/features/system/selectors";
+import Chip from "./Chip";
+
+const mapStateToProps = (state, ownProps) => ({
+  customTypes: customTypesEntitiesSelector(state),
+  nationality: customTypeByIdentitySelector(state, {
+    cstmTypDtlTxt: ownProps?.pair?.value,
+  }),
+  countries: countriesEntitiesSelector(state),
+  trustees: trusteesEntitiesSelector(state),
+  schemes: schemesEntitiesSelector(state),
+});
+
+export default connect(mapStateToProps, null)(Chip);
