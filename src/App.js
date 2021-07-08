076dfea828911d3eace6619ab1ca6dfb1d67e2c4
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-07-08 17:48:13 +0800

    Reset page scrollbar when page changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { createRoutes } from "@components/misc";
 import { appRoutes } from "@routes";
 
-const App = () => createRoutes(appRoutes);
+const routes = createRoutes(appRoutes);
+
+const App = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    return history.listen(() => {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "auto",
+      });
+    });
+  }, [history]);
+
+  return routes;
+};
 
 export default App;
