4614c8abdbef9ebe32cba53115e446bff1fcd1f7
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-07-08 10:00:15 +0800

    Add contact persons state + adapter

--- src/redux/features/enrollmentEmployer/state.js
+++ src/redux/features/enrollmentEmployer/state.js
@@ -4,6 +4,10 @@ export const employersAdapter = createEntityAdapter({
   selectId: (employer) => employer.companyName,
 });
 
+export const contactPersonsAdapter = createEntityAdapter({
+  selectId: (contactPerson) => contactPerson.cntctPrsnUuid,
+});
+
 export const schemesAdapter = createEntityAdapter({
   selectId: (scheme) => scheme.name,
   sortComparer: (a, b) => a.name.localeCompare(b.name),
@@ -26,6 +30,7 @@ export const initialState = enrollmentEmployer.getInitialState({
   selectedSchemeUUID: null,
   selectedPayrollGroupUUID: null,
   employers: employersAdapter.getInitialState(),
+  contactPersons: contactPersonsAdapter.getInitialState(),
   schemes: schemesAdapter.getInitialState(),
   trustees: trusteesAdapter.getInitialState(),
 });
