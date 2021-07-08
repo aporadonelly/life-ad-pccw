86b8561eed2d1c3ebde16cb15605d3577840fb2e
pccw-nellyaporado <NellyRose.T.Aporado@pccw.com>
2021-07-08 16:25:41 +0800

    Working on displaying payroll group contact person

--- src/redux/features/enrollmentEmployer/state.js
+++ src/redux/features/enrollmentEmployer/state.js
@@ -8,6 +8,11 @@ export const contactPersonsAdapter = createEntityAdapter({
   selectId: (contactPerson) => contactPerson.cntctPrsnUuid,
 });
 
+export const payrollGroupContactPersonsAdapter = createEntityAdapter({
+  selectId: (payrollGroupContactPerson) =>
+    payrollGroupContactPerson.cmpnyBrnchCd,
+});
+
 export const schemesAdapter = createEntityAdapter({
   selectId: (scheme) => scheme.name,
   sortComparer: (a, b) => a.name.localeCompare(b.name),
@@ -31,6 +36,7 @@ export const initialState = enrollmentEmployer.getInitialState({
   selectedPayrollGroupUUID: null,
   employers: employersAdapter.getInitialState(),
   contactPersons: contactPersonsAdapter.getInitialState(),
+  payrollGroupContactPersons: payrollGroupContactPersonsAdapter.getInitialState(),
   schemes: schemesAdapter.getInitialState(),
   trustees: trusteesAdapter.getInitialState(),
 });
