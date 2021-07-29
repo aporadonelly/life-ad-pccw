1b545e05150d553a6da6a1fa80ac9f0db220e6a0
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-07-29 13:51:50 +0800

    Life-297: Implement tab context api

--- src/pages/DataModification/DataModification.js
+++ src/pages/DataModification/DataModification.js
@@ -1,18 +1,16 @@
+import { useState } from "react";
 import { Page } from "@containers";
 import { PageInner } from "@components/layout";
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Box, Tab, Typography } from "@material-ui/core";
+import { TabContext, TabList, TabPanel } from "@material-ui/lab";
 import SupportingDocuments from "./SupportingDocuments";
-import { Grid } from "@material-ui/core";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import { useState } from "react";
-import { Typography, Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    display: 'flex',
+    display: "flex",
     // height: 224,
   },
   rootTab: {
@@ -21,12 +19,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const DataModification = (props) => {
-  const [index, setValue] = useState(0);
+  const [activeTab, setActiveTab] = useState("Section 8");
   const classes = useStyles();
 
-  const onChange = (value, newValue) => {
-    // console.log('newValue :' + newValue);
-    setValue(newValue);
+  const handleChange = (value, newValue) => {
+    setActiveTab(newValue);
   };
 
   const a11yProps = (index) => {
@@ -40,59 +37,53 @@ const DataModification = (props) => {
   return (
     <Page>
       <PageInner>
-        <div className={classes.root}>
-          <Grid xs={2}>
-            <Tabs
-              indicatorColor="none"
-              orientation="vertical"
-              variant="scrollable"
-              value={index}
-              onChange={onChange}
-              aria-label="Section Tab"
-            >
-              <Tab
-                label={TitleTabs("1:", "Personal Details")}
-                {...a11yProps(0)}
-              />
-              <Tab
-                className={{root: classes.rootTab}}
-                label={TitleTabs("2:", "Contact Information")}
-                {...a11yProps(1)}
-              />
-              <Tab
-                label={TitleTabs("3:", "Communication Preferences")}
-                {...a11yProps(2)}
-              />
-              <Tab
-                className={{root: classes.rootTab}}
-                label={TitleTabs("4:", "MPF Accounts Information")}
-                {...a11yProps(3)}
-              />
-              <Tab
-                label={TitleTabs("5:", "Payment Method")}
-                {...a11yProps(4)}
-              />
-              <Tab
-                className={{root: classes.rootTab}}
-                label={TitleTabs("6:", "CRS Information")}
-                {...a11yProps(5)}
-              />
-              <Tab
-                label={TitleTabs("7:", "Others Details")}
-                {...a11yProps(6)}
-              />
-              <Tab
-                className={{root: classes.rootTab}}
-                label={TitleTabs("8:", "Upload Supporting Documents")}
-                {...a11yProps(7)}
-              />
-              <Tab label={TitleTabs("9:", "Confirmation")} {...a11yProps(8)} />
-            </Tabs>
+        <TabContext value={activeTab}>
+          <Grid container>
+            <Grid item xs={2}>
+              <TabList
+                indicatorColor="none"
+                orientation="vertical"
+                variant="scrollable"
+                onChange={handleChange}
+                aria-label="Section Tab"
+              >
+                <Tab label={TitleTabs("1:", "Personal Details")} />
+                <Tab
+                  className={{ root: classes.rootTab }}
+                  label={TitleTabs("2:", "Contact Information")}
+                />
+                <Tab
+                  label={TitleTabs("3:", "Communication Preferences")}
+                  {...a11yProps(2)}
+                />
+                <Tab
+                  className={{ root: classes.rootTab }}
+                  label={TitleTabs("4:", "MPF Accounts Information")}
+                />
+                <Tab label={TitleTabs("5:", "Payment Method")} />
+                <Tab
+                  className={{ root: classes.rootTab }}
+                  label={TitleTabs("6:", "CRS Information")}
+                />
+                <Tab label={TitleTabs("7:", "Others Details")} />
+                <Tab
+                  className={{ root: classes.rootTab }}
+                  label={TitleTabs("8:", "Upload Supporting Documents")}
+                  value="Section 8"
+                />
+                <Tab
+                  label={TitleTabs("9:", "Confirmation")}
+                  {...a11yProps(8)}
+                />
+              </TabList>
+            </Grid>
+            <Grid item xs={10}>
+              <TabPanel value="Section 8">
+                <SupportingDocuments />
+              </TabPanel>
+            </Grid>
           </Grid>
-          
-          {/* SECTION - 8 */}
-          <SupportingDocuments index={index} />
-        </div>
+        </TabContext>
       </PageInner>
     </Page>
   );
@@ -115,4 +106,5 @@ const TitleTabs = (section, title) => (
     </Typography>
   </Box>
 );
+
 export default DataModification;
