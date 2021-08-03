fbf367dda415809e4ada1ceff5341d0b1004d557
Anthony.C.Tinio@pccw.com <Anthony.C.Tinio@pccw.com>
2021-08-03 17:58:47 +0800

    Life-316: Create a back button UI

--- src/pages/Employers/Enrollment/PayrollGroup/PayrollGroup.js
+++ src/pages/Employers/Enrollment/PayrollGroup/PayrollGroup.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { useEffect } from "react";
-import { Grid } from "@material-ui/core";
+import React, { useEffect } from "react";
+import { Grid, Button } from "@material-ui/core";
 import Information from "./Information";
 import ContactPersonList from "./ContactPersonList";
 import GradeList from "./GradeList";
+import { useTranslation } from "react-i18next";
 
-const PayrollGroup = () => {
+const PayrollGroup = ({ push }) => {
+  const { t } = useTranslation(["typography", "form", "table", "button"]);
+
+  // useEffect(() => {
+  //   push();
+  // }, [push]);
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
@@ -16,6 +22,14 @@ const PayrollGroup = () => {
       </Grid>
       <Grid item xs={12}>
         <GradeList />
+      </Grid>
+      <Grid item xs={12} align="right">
+        <Button
+          data-testid="back-btn"
+          onClick={() => push("/employers/enrollment/information")}
+        >
+          {t("button:back")}
+        </Button>
       </Grid>
     </Grid>
   );
