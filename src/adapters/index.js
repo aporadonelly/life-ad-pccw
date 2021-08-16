fe083950d69b527db1970a393c558190b9148d61
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-08-16 10:06:40 +0800

    Add enrollment employee adapter

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -2,6 +2,7 @@ export { default as systemAdapter } from "./system.adapter";
 export { default as userAdapter } from "./user.adapter";
 export { default as employeesAdapter } from "./members.adapter";
 export { default as terminationAdapter } from "./emp_termination.adapter";
+export { default as enrollmentEmployee } from "./enrollmentEmployee.adapter";
 export { default as enrollmentEmployer } from "./enrollmentEmployer.adapter";
 export { default as enrollmentSelfEmployed } from "./enrollmentSelfEmployed.adapter";
 export { default as mlfub } from "./mlfub.adapter";
