204122af6613d1798b20701ab64afb05f891f328
Jason Nares <jason.b.nares@pccw.com>
2021-08-17 15:48:52 +0800

    Life-386: Renamed file from enrollmentCEE to enrollmentCasualEmployee.

--- src/adapters/enrollmentCasualEmployee.adapter.js
+++ src/adapters/enrollmentCasualEmployee.adapter.js
@@ -1,12 +1,12 @@
 import AxiosAdapter from "./axios.adapter";
 
-class EnrollmentCEE extends AxiosAdapter {
+class EnrollmentCasualEmployee extends AxiosAdapter {
   getIndAccntLst(payload) {
     const { indUuid } = payload;
     return this.instance.get("/getIndAccntLst", { params: { indUuid } });
   }
 }
 
-export default new EnrollmentCEE({
+export default new EnrollmentCasualEmployee({
   baseURL: process.env.REACT_APP_ENROLLMENT_CEE_BASE_URL,
 });
--- src/adapters/index.js
+++ src/adapters/index.js
@@ -6,4 +6,4 @@ export { default as enrollmentEmployer } from "./enrollmentEmployer.adapter";
 export { default as enrollmentSelfEmployed } from "./enrollmentSelfEmployed.adapter";
 export { default as mlfub } from "./mlfub.adapter";
 export { default as registrationEmployer } from "./registrationEmployer.adapter";
-export { default as enrollmentCEE } from "./enrollmentCEE.adapter";
+export { default as enrollmentCasualEmployee } from "./enrollmentCasualEmployee.adapter";
