495df181632dc03a1289a80a50419442cc7d5908
Jason Nares <jason.b.nares@pccw.com>
2021-08-17 14:32:45 +0800

    Life-386: Implement/connect page to redux.

--- src/pages/Members/Enquiry/AccountTypes/index.js
+++ src/pages/Members/Enquiry/AccountTypes/index.js
@@ -1,1 +1,26 @@
-export { default } from "./AccountTypes";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+  indAccntLstSelector,
+  isLoadingSelector,
+} from "@redux/features/enrollmentCEE/selectors";
+import { getIndAccntLst } from "@redux/features/enrollmentCEE/actions";
+import AccountTypes from "./AccountTypes";
+import { push } from "connected-react-router";
+
+const mapStateToProps = (state) => ({
+  indAccntLst: indAccntLstSelector(state),
+  isLoading: isLoadingSelector(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators(
+    {
+      getIndAccntLst,
+      push,
+    },
+    dispatch
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountTypes);
