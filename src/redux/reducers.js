a307dbe1647ee307e173af1a690f53328d19912c
Archy Villanueva <Archy.T.Villanueva@pccw.com>
2021-08-18 14:25:27 +0800

    Remove unsued reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,6 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import system from "./features/system/reducer";
 import user from "./features/user/reducer";
-import membersReducer from "./features/members/reducer";
 import employeeTermination from "./features/employees/termination/reducer";
 import enrollmentEmployer from "./features/enrollmentEmployer/reducer";
 import mlfub from "./features/mlfub/reducer";
@@ -13,7 +12,6 @@ import enrollmentCasualEmployee from "./features/enrollmentCasualEmployee/reduce
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
-    employees: membersReducer,
     system,
     user,
     registrationEmployer,
