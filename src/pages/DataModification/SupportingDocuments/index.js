5bdaefa686d720417401608cd59a1f0307628eaa
Berwin <bhdrms0614@gmail.com>
2021-08-20 13:52:58 +0800

    Life-297: add selectors, dispatch and state props

--- src/pages/DataModification/SupportingDocuments/index.js
+++ src/pages/DataModification/SupportingDocuments/index.js
@@ -1,1 +1,28 @@
-export { default } from "./SupportingDocuments";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { uploadDocuments } from "@redux/features/dataModification/actions";
+import SupportingDocuments from "./SupportingDocuments";
+import {
+  isLoadingSelector,
+  errorSelector,
+} from "@redux/features/dataModification/selectors";
+
+const mapStateToProps = (state) => ({
+  isLoading: isLoadingSelector(state),
+  error: errorSelector(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators(
+    {
+      uploadDocuments,
+    },
+    dispatch
+  ),
+});
+
+// export { default } from "./SupportingDocuments";
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SupportingDocuments);
