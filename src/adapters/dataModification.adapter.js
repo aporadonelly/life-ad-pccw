996a64c39a0f718f73299e4d96ad49230b08bb84
Berwin <bhdrms0614@gmail.com>
2021-08-20 13:59:01 +0800

    Life-297: add vldUpldDoc adapter

--- src/adapters/dataModification.adapter.js
+++ src/adapters/dataModification.adapter.js
@@ -1,6 +1,11 @@
 import AxiosAdapter from "./axios.adapter";
 
-class DataModification extends AxiosAdapter {}
+class DataModification extends AxiosAdapter {
+  // VALIDATE DOCUMENTS UPLOAD
+  validateDocuments(payload) {
+    return this.instance.post("/vldUpldDoc", payload);
+  }
+}
 
 export default new DataModification({
   baseURL: process.env.REACT_APP_DATA_MODIFICATION_BASE_URL,
